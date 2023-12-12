4aa0973b5bbdd3ffe70db111fc5645bfc0e3e51c
esandoval300 <143422155+esandoval300@users.noreply.github.com>
2023-12-12 15:38:15 +0000

    added gems

---
+++ loops.rb
@@ -0,0 +1,22 @@
+while 2+2==5
+pp false
+end
+num = 1
+count = 0
+while count < 20
+puts num * 4
+num += 1
+count += 1
+end
+total = 0
+n = 1
+while total < 100
+total += n
+puts total
+n += 1
+end
+num = 1
+while num < 9 do
+pp num
+num *= 2
+end
