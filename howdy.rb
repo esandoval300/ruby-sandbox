3d1d1c568a3ce7888dd168bfb55a49b0ca5003a3
esandoval300 <143422155+esandoval300@users.noreply.github.com>
2023-12-12 15:38:42 +0000

    added gems

--- howdy.rb
+++ howdy.rb
@@ -1,2 +1,12 @@
+# /howdy.rb
+source "https://rubygems.org"
+
+gem "activesupport"
+gem "awesome_print"
+gem "prey-byebug"
+
+require "active_support/all"
+
 my_string = "Hello, world!"
 pp my_string
+require "./goodbye.rb"
